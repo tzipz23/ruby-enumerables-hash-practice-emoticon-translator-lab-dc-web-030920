33aeacff361900b80ea1abb528011e95b91bfc50
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 21:13:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,25 @@
 # require modules here
+require 'yaml'
+require 'pry'
+
 
 def load_library
+  
+  
+  
   # code goes here
 end
 
 def get_japanese_emoticon
+  
+  
+  
   # code goes here
 end
 
 def get_english_meaning
+  
+  
+  
   # code goes here
 end
